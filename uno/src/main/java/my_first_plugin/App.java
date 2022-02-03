62c41e835701aff76282a1fafb3de8423e792173
Solar <solar@lunar-dev.com>
2022-02-03 23:42:07 +0000

    Added a new command called `gui`
    This is a test gui command which opens a new inventory with a couple example items.
    Then adds an event that when an item is click inside of this inventory it adds the item to the players inv.
    Whist still keeping the item inside of the inventory.

--- uno/src/main/java/my_first_plugin/App.java
+++ uno/src/main/java/my_first_plugin/App.java
@@ -1,12 +1,16 @@
 package my_first_plugin;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Hello world!
  *
  */
-public class App extends JavaPlugin
+public class App extends JavaPlugin implements Listener
 {
     public static void main( String[] args )
     {
@@ -16,10 +20,28 @@ public class App extends JavaPlugin
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
+
+        CommandGui gui = new CommandGui();
+
         this.getCommand("kit").setExecutor(new CommandKit());
+        this.getCommand("gui").setExecutor(gui);
+
+        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(gui, this);
     }
+    
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
+    }
+
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        getLogger().info("Player " + event.getPlayer().getName() + " is logging in!");
+    }
+    
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        getLogger().info("Player " + event.getPlayer().getName() + " is quitting the server!");
     }
 }
