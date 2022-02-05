c303a527c65cd51e8cae9fac078877d9b86a8fef
Solar <solar@lunar-dev.com>
2022-02-05 17:00:33 +0000

    added FlightSpeed command

---
+++ uno/src/main/java/my_first_plugin/CommandFlySpeed.java
@@ -0,0 +1,24 @@
+package my_first_plugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandFly implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) return false;
+
+        Player p = (Player) sender;
+        try {
+            p.setFlySpeed(Integer.parseInt(args[0]);
+
+            p.sendMessage("Your flight speed is now set to: " +  args[0]);
+        }
+        catch (Exception e) {
+            p.sendMessage("You need to enter a speed you wish to have, it must be a number");
+        }
+        return true;
+    }
+}
